3ac2fd47aed660b0a5d3b01abc253b594605bb79
偉國 曾 <108111113@mail.aeust.edu.tw>
2021-11-12 10:45:14 +0800

    2-1

--- 110_1Mid/Sample2.aspx
+++ 110_1Mid/Sample2.aspx
@@ -10,6 +10,31 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <h1>維修項目表單</h1>
+            <asp:Label ID="Label1" runat="server" Text="編號" Font-Size="X-Large"></asp:Label>
+            <asp:Label ID="lb_Id" runat="server" Text=""></asp:Label>
+            <br/>
+            <asp:Label ID="Label2" runat="server" Text="聯絡方式" Font-Size="X-Large"></asp:Label>
+            <asp:DropDownList ID="dd1_Type" runat="server" AutoPostBack="True" OnSelectedIndexChanged="dd1_Type_SelectedIndexChanged">
+                <asp:ListItem>手機</asp:ListItem>
+                <asp:ListItem>市話</asp:ListItem>
+            </asp:DropDownList>
+            <br/>
+            <asp:DropDownList ID="dd1_ZoneCode" runat="server" AutoPostBack="True" OnSelectedIndexChanged="dd1_ZoneCode_SelectedIndexChanged"></asp:DropDownList>
+            <asp:TextBox ID="tb_Number" runat="server"></asp:TextBox>
+            <br/>
+            <asp:Label ID="Label3" runat="server" Text="地址" Font-Size="X-Large"></asp:Label>
+            <asp:DropDownList ID="dd1_City" runat="server"></asp:DropDownList>
+            <asp:TextBox ID="tb_Add" runat="server"></asp:TextBox>
+            <br/>
+            <asp:Label ID="Label4" runat="server" Text="說明" Font-Size="X-Large"></asp:Label>
+            <asp:RadioButtonList ID="rd_Des" runat="server" AutoPostBack="True">
+                <asp:ListItem Selected="True">否</asp:ListItem>
+                <asp:ListItem>是</asp:ListItem>
+            </asp:RadioButtonList>
+            <asp:TextBox ID="tb_Des" runat="server" Width="400px" Visible="False"></asp:TextBox>
+            <asp:Button ID="btn_Submit" runat="server" Text="Button" />
+            <asp:Label ID="lb_Msg" runat="server" Text=""></asp:Label>
         </div>
     </form>
 </body>
--- 110_1Mid/Sample2.aspx.cs
+++ 110_1Mid/Sample2.aspx.cs
@@ -7,8 +7,24 @@ using System.Web.UI.WebControls;
 
 namespace _110_1Mid {
     public partial class Sample2 : System.Web.UI.Page {
+        String[,] sa_ZoneCode = new string[2,3] { {"0911","0921","0960" }, {"02","04","07" } };
+        String[] sa_City = new string[6] {"台北市","新北市", "台中市", "台中縣", "高雄市", "高雄縣" };
+        String[,] sa_City2 = new string[3, 2] { {"台北市", "新北市"},
+                                                {"台中市", "台中縣"},
+                                                {"高雄市", "高雄縣"} };
         protected void Page_Load(object sender, EventArgs e) {
-
+            if (!IsPostBack) { 
+                lb_Id.Text = mt_GenVeriStr();
+                ListItem o_li;
+                for (int i = 0;i <= 2;i++) { 
+                    o_li = new ListItem(sa_ZoneCode[0,i], sa_ZoneCode[0, i]);
+                    dd1_ZoneCode.Items.Add(o_li);
+                }
+                for (int i = 0;i <= 5;i++) { 
+                    o_li = new ListItem(sa_City[i], sa_City[i]);
+                    dd1_City.Items.Add(o_li);
+                }
+            }
         }
 
         // To generate a 4 digital number
@@ -20,6 +36,62 @@ namespace _110_1Mid {
                 s_Res = s_Res + i_Tmp.ToString();
             }
             return s_Res;
+        }
+
+        protected void dd1_Type_SelectedIndexChanged(object sender, EventArgs e){
+            dd1_ZoneCode.Items.Clear();
+            dd1_City.Items.Clear();
+            ListItem o_li;
+            if (dd1_Type.SelectedValue == "手機") {
+                for (int i = 0;i <= 2;i++) { 
+                    o_li = new ListItem(sa_ZoneCode[0,i], sa_ZoneCode[0, i]);
+                    dd1_ZoneCode.Items.Add(o_li);
+                }
+                for (int i = 0;i <= 5;i++) { 
+                    o_li = new ListItem(sa_City[i], sa_City[i]);
+                    dd1_City.Items.Add(o_li);
+                }
+            }else{ 
+                for (int i = 0;i <= 2;i++) { 
+                    o_li = new ListItem(sa_ZoneCode[1,i], sa_ZoneCode[1, i]);
+                    dd1_ZoneCode.Items.Add(o_li);
+                }
+                for (int i = 0;i <= 1;i++) { 
+                    o_li = new ListItem(sa_City2[0,i], sa_City2[0,i]);
+                    dd1_City.Items.Add(o_li);
+                }
+            }
+        }
+
+        protected void dd1_ZoneCode_SelectedIndexChanged(object sender, EventArgs e){
+            dd1_City.Items.Clear();
+            ListItem o_li;
+            switch (dd1_ZoneCode.SelectedValue) {
+                case "02":
+                    for (int i = 0;i <= 1;i++) { 
+                        o_li = new ListItem(sa_City2[0,i], sa_City2[0,i]);
+                        dd1_City.Items.Add(o_li);
+                    }
+                    break;
+                case "04":
+                    for (int i = 0;i <= 1;i++) { 
+                        o_li = new ListItem(sa_City2[1,i], sa_City2[1,i]);
+                        dd1_City.Items.Add(o_li);
+                    }
+                    break;
+                case "07":
+                    for (int i = 0;i <= 1;i++) { 
+                        o_li = new ListItem(sa_City2[2,i], sa_City2[2,i]);
+                        dd1_City.Items.Add(o_li);
+                    }
+                    break;
+                default:
+                    for (int i = 0;i <= 5;i++) { 
+                        o_li = new ListItem(sa_City[i], sa_City[i]);
+                        dd1_City.Items.Add(o_li);
+                    }
+                    break;
+            }
         }
     }
 }
